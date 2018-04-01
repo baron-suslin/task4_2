3adcbe5292089874547370d41721771701e9a116
Baron Suslin <baron-suslin@rambler.ru>
2018-04-01 12:07:21 +0300

    v1.1

--- ntp_verify.sh
+++ ntp_verify.sh
@@ -16,7 +16,7 @@ if [ $? == 1 ];
   echo "NOTICE: /etc/ntp.conf was changed. Calculated diff:"
   diff -u0 /etc/ntp.conf.bak /etc/ntp.conf
   cat /etc/ntp.conf.bak > /etc/ntp.conf
-  /etc/init.d/ntp restart > /dev/null
+  /etc/init.d/ntp restart
 fi;
 
 exit 0;
